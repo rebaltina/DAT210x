4a6f6e19ecf3834f5d53da8da4335397148f9a55
VALENTINA DEBATTISTI <valentina.debattisti@gmail.com>
2016-11-02 18:40:31 -0400

    assignments 2-4-5 Module2 completed
    assignments 2-4-5 Module2 completed

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -8,11 +8,10 @@ print(df)
 
 # TODO: Print the results of the .describe() method
 #
-# .. your code here ..
-
+print(df.describe())
 
 
 # TODO: Figure out which indexing method you need to
 # use in order to index your dataframe with: [2:4,'col3']
 # And print the results
-#
+print(df.loc[2:4, 'col3'])
